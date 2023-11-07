efc9ac539c616bf87dc2b67585114acccea5b168
Mika <38019054+mikap0703@users.noreply.github.com>
2023-11-07 23:47:48 +0100

    configuring tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: vitePreprocess()
 };
