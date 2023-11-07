37a5fc2a83954e58e766ceacf2cd1d21979ed116
Mika <38019054+mikap0703@users.noreply.github.com>
2023-11-07 02:42:57 +0100

    adding server.js

---
+++ src/routes/api/current-images/+server.js
@@ -0,0 +1,50 @@
+// src/routes/api/images.js
+import fs from 'fs/promises'; // Use the promise-based version of 'fs'.
+import { join } from 'path';
+
+export async function GET(request) {
+  const imageDirectory = 'static/img'; // Set your base image directory
+
+  async function getImagesInDirectory(directory) {
+    const dirImages = [];
+
+    try {
+      const files = await fs.readdir(directory); // Use await to ensure async completion.
+
+      for (const file of files) {
+        const regex = /(\d+)(?:-(\d+))?\.(\w+)$/;
+        const match = file.match(regex);
+
+        if (match) {
+          const id = match[1];
+          const duration = match[2] ? parseInt(match[2], 10) : 10;
+
+          const data = await fs.readFile(join(directory, file)); // Use await for reading the file.
+
+          // Convert the image data to a base64-encoded string.
+          const base64Image = Buffer.from(data).toString('base64');
+
+          dirImages.push({
+            img: base64Image, // No need to stringify base64Image as it's already a string.
+            order: id,
+            duration: duration,
+          });
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    return dirImages;
+  }
+
+  const images = [];
+
+  images.push(...await getImagesInDirectory(join(imageDirectory, 'default'))); // Use await here.
+
+  const dateFolder = new Date().toISOString().split('T')[0];
+  images.push(...await getImagesInDirectory(join(imageDirectory, dateFolder))); // Use await here.
+
+  // Return a response with the array of images
+  return new Response(JSON.stringify(images));
+}
