963e3ded59b176c155fd8d4d72dab9ce969ae614
Mika <38019054+mikap0703@users.noreply.github.com>
2023-11-28 14:09:57 +0100

    adding check for file path to prevent exploits

--- src/routes/api/current-images/+server.js
+++ src/routes/api/current-images/+server.js
@@ -73,8 +73,10 @@ export async function POST(request) {
   images.push(...await getImagesInDirectory(join(imageDirectory, 'default'))); // Use await here.
 
   const formattedDate = requestData.date
-  // possible exploit
-  images.push(...await getImagesInDirectory(join(imageDirectory, formattedDate))); // Use await here.
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  if (regex.test(formattedDate)) {
+    images.push(...await getImagesInDirectory(join(imageDirectory, formattedDate))); // Use await here.
+  }
 
   // Return a response with the array of images
   return new Response(JSON.stringify({
